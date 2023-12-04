b2499b9f40b883a921436b1fcbe3049efb9d89d8
Sayani Nethmi <nethmihewage2002@gmail.com>
2023-12-05 00:15:54 +0530

    operators added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 function App() {
+
+
+
   return (
     <div className="App">
+        <div className="calculator">
+            <div className="display">
+                <span>(0)</span>0
+
+            </div>
+            <div className="operators">
+                <button>/</button>
+                <button>*</button>
+                <button>+</button>
+                <button>-</button>
+
+                <button>DEL</button>
+            </div>
+            <div className="digits">
+                <button>0</button>
+                <button>.</button>
+                <button>=</button>
+            </div>
+        </div>
     </div>
   );
 }
